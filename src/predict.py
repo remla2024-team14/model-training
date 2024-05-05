549f4e9b0b13b73bcd9e0199883861b7ff3a1cf7
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2024-05-05 12:17:14 +0200

    Add docstrings

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,5 @@
+"""Module for evaluating the trained model on test set"""
+
 from os.path import join
 
 import numpy as np
@@ -8,12 +10,30 @@ from config_reader import ConfigReader
 directories = ConfigReader().params["directories"]
 MODEL_PATH, TOKENIZED_PATH = directories["model_path"], directories["tokenized_outputs_dir"]
 
+
 def load_variable(filename):
+    """
+    Load a variable from a text file.
+
+    Args:
+        filename (str): The name of the file to load from (without extension).
+
+    Returns:
+        numpy.ndarray: The variable loaded from the file.
+    """
     file_path = join(TOKENIZED_PATH, filename + ".txt")
     return np.loadtxt(file_path)
 
 
 def load_test_dataset():
+    """
+    Load the test dataset.
+
+    Returns:
+        tuple: A tuple containing test data and labels:
+            - x_test (numpy.ndarray): Tokenized and padded sequences of input test data.
+            - y_test (numpy.ndarray): Encoded labels for test data.
+    """
     x_test = load_variable("x_test")
     y_test = load_variable("y_test")
 
@@ -21,11 +41,25 @@ def load_test_dataset():
 
 
 def load_tf_model():
+    """
+    Load the trained TensorFlow model.
+
+    Returns:
+        keras.models.Model: The loaded TensorFlow model.
+    """
     model = load_model(MODEL_PATH)
     return model
 
 
 def predict(model, x_test, y_test):
+    """
+    Make predictions using the trained model and print evaluation metrics.
+
+    Args:
+        model (keras.models.Model): The trained model.
+        x_test (numpy.ndarray): Tokenized and padded sequences of input test data.
+        y_test (numpy.ndarray): Encoded labels for test data.
+    """
     y_pred = model.predict(x_test, batch_size=1000)
 
     # Convert predicted probabilities to binary labels
@@ -42,11 +76,16 @@ def predict(model, x_test, y_test):
     print('Confusion Matrix:', confusion_mat)
     print('Accuracy:', accuracy_score(y_test, y_pred_binary))
 
+
 def main():
+    """
+    Load the trained model, test dataset, and make predictions.
+    """
     model = load_tf_model()
     x_test, y_test = load_test_dataset()
 
     predict(model, x_test, y_test)
+
 
 if __name__ == "__main__":
     main()
--- src/tokenize_data.py
+++ src/tokenize_data.py
@@ -1,3 +1,5 @@
+"""Module for tokenizing raw train and test datasets"""
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
@@ -16,19 +18,45 @@ FILE_NAMES = ["x_train", "y_train", "x_test", "y_test", "x_val", "y_val"]
 
 
 def _load_data(file_dir):
-    vars = {}
+    """Load raw data (see FILE_NAMES) to be tokenized into dictionary.
+
+    Args:
+        file_dir (str): The directory path containing the raw train, test and val datasets.
+
+    Returns:
+        dict: A dictionary where keys are the FILE_NAMES (with 'raw_' prefix added) and
+            values are the contents of the corresponding text files loaded as JSON.
+    """
+    data_dict = {}
 
     for filename in FILE_NAMES:
         filename = "raw_" + filename
         with open(join(file_dir, filename + ".txt"), 'r') as filehandle:
-            vars[filename] = json.load(filehandle)
+            data_dict[filename] = json.load(filehandle)
 
-    return vars["raw_x_train"], vars["raw_x_val"], vars["raw_y_train"], vars["raw_y_val"], vars["raw_x_test"], vars[
-        "raw_y_test"]
+    return data_dict
 
 
 def tokenize(dataset_input_dir):
-    raw_x_train, raw_x_val, raw_y_train, raw_y_val, raw_x_test, raw_y_test = _load_data(dataset_input_dir)
+    """Tokenize the raw text data from the given directory.
+
+    Args:
+        dataset_input_dir (str): The directory path containing the raw datasets.
+
+    Returns:
+        tuple: A tuple containing the tokenized data and associated information:
+            - x_train (numpy.ndarray): Tokenized and padded sequences of input training data.
+            - y_train (numpy.ndarray): Encoded labels for training data.
+            - x_val (numpy.ndarray): Tokenized and padded sequences of input validation data.
+            - y_val (numpy.ndarray): Encoded labels for validation data.
+            - x_test (numpy.ndarray): Tokenized and padded sequences of input test data.
+            - y_test (numpy.ndarray): Encoded labels for test data.
+            - char_index (dict): A dictionary mapping characters to their indices.
+    """
+    data_dict = _load_data(dataset_input_dir)
+    raw_x_train, raw_y_train = data_dict['raw_x_train'], data_dict['raw_y_train']
+    raw_x_val, raw_y_val = data_dict['raw_x_val'], data_dict['raw_y_val']
+    raw_x_test, raw_y_test = data_dict['raw_x_test'], data_dict['raw_y_test']
 
     tokenizer = Tokenizer(lower=True, char_level=True, oov_token='-n-')
     tokenizer.fit_on_texts(raw_x_train + raw_x_val + raw_x_test)
@@ -48,7 +76,14 @@ def tokenize(dataset_input_dir):
 
 
 def main():
-    # Ensure input and output directories exist
+    """
+    Main function for saving tokenized data.
+
+    This function performs the following steps:
+    1. Ensures input and output directories exist.
+    2. Tokenizes input data.
+    3. Saves tokenized data and associated information.
+    """
     os.makedirs(INPUTS_DIR, exist_ok=True)
     os.makedirs(OUTPUTS_DIR, exist_ok=True)
     x_train, y_train, x_val, y_val, x_test, y_test, char_index = tokenize(INPUTS_DIR)
@@ -64,6 +99,17 @@ def main():
 
 
 def save_variable(variable, filename):
+    """
+    Save a variable to a text file.
+
+    Args:
+        variable: The variable to save.
+        filename (str): The name of the file to save to (without extension).
+
+    Notes:
+        The file will be saved in the OUTPUTS_DIR directory.
+        The variable will be saved in text format.
+    """
     np.savetxt(join(OUTPUTS_DIR, filename + ".txt"), variable, fmt='%d')
 
 
