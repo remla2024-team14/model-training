1b19b22030849e0b6b8b18a0ff05f4566765c05b
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-12 15:39:30 +0200

    Add debugging line

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,14 @@
 from config_reader import ConfigReader
-# from os.path import join
-import os
+from os.path import join
 
-print("Current Working Directory:", os.getcwd())
 directories = ConfigReader().params["directories"]
 TOKENIZED_PATH = directories["tokenized_outputs_dir"]
-# RAW_DATA_PATH = directories["raw_outputs_dir"]
+RAW_DATA_PATH = directories["raw_outputs_dir"]
 
 
 def load_variable(filename):
-    base_path = "/home/runner/work/model-training/model-training/"
-    file_path = os.path.join(base_path, "outputs/raw", filename)
-    # file_path = join(RAW_DATA_PATH, filename)
+    file_path = join(RAW_DATA_PATH, filename)
+    print(f"Attempting to load file at: {file_path}")  # Debugging line
     # data = pd.read_csv(file_path, header=None)
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read().splitlines()
