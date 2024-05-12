4471c090e2d41db9f5022b00e8c7d3d72c1e5ecc
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-12 15:56:20 +0200

    FileNotFoundError

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,9 @@ RAW_DATA_PATH = directories["raw_outputs_dir"]
 
 
 def load_variable(filename):
-    base_dir = dirname(realpath(__file__))  # 获取当前文件的绝对路径
+    base_dir = dirname(dirname(dirname(realpath(__file__))))
     file_path = join(base_dir, 'outputs', 'raw', filename)
+    print("Trying to open file at:", file_path)
     # file_path = join(RAW_DATA_PATH, filename)
     # print(f"Attempting to load file at: {file_path}")  # Debugging line
     # data = pd.read_csv(file_path, header=None)
