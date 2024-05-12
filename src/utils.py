31879c41433570098828ac51aae4fc42fd021d3f
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-12 16:12:05 +0200

    fix FileNotFoundError

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from config_reader import ConfigReader
-from os.path import join, dirname, realpath
-# import os
+from os.path import join
+
 
 directories = ConfigReader().params["directories"]
 TOKENIZED_PATH = directories["tokenized_outputs_dir"]
@@ -8,10 +8,7 @@ RAW_DATA_PATH = directories["raw_outputs_dir"]
 
 
 def load_variable(filename):
-    base_dir = dirname(dirname(dirname(realpath(__file__))))
-    file_path = join(base_dir, 'outputs', 'raw', filename)
-    print("Trying to open file at:", file_path)
-    # file_path = join(RAW_DATA_PATH, filename)
+    file_path = join(RAW_DATA_PATH, filename)
     # print(f"Attempting to load file at: {file_path}")  # Debugging line
     # data = pd.read_csv(file_path, header=None)
     with open(file_path, 'r', encoding='utf-8') as file:
