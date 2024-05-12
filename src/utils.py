842911b6b510a4da6c3a14a0856eac92a7abdd81
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-12 14:41:20 +0200

    improve code quality

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ RAW_DATA_PATH = directories["raw_outputs_dir"]
 
 def load_variable(filename):
     file_path = join(RAW_DATA_PATH, filename)
-    #data = pd.read_csv(file_path, header=None)
+    # data = pd.read_csv(file_path, header=None)
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read().splitlines()
     return data
