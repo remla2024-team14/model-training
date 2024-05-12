100f559f6b48ee3b49e2a838c320b70a5c33982a
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-12 15:19:18 +0200

    resolve FileNotFoundError

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,17 @@
 from config_reader import ConfigReader
 from os.path import join
+import os
 
+print("Current Working Directory:", os.getcwd())
 directories = ConfigReader().params["directories"]
 TOKENIZED_PATH = directories["tokenized_outputs_dir"]
-RAW_DATA_PATH = directories["raw_outputs_dir"]
+# RAW_DATA_PATH = directories["raw_outputs_dir"]
 
 
 def load_variable(filename):
-    file_path = join(RAW_DATA_PATH, filename)
+    base_path = "/home/runner/work/model-training/model-training/"
+    file_path = os.path.join(base_path, "outputs/raw", filename)
+    # file_path = join(RAW_DATA_PATH, filename)
     # data = pd.read_csv(file_path, header=None)
     with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read().splitlines()
