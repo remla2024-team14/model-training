7f4de095f494ad8e0d05422bf60abff46808dcfb
07Liyang <152295290+07Liyang@users.noreply.github.com>
2024-05-29 09:00:59 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from src.config_reader import ConfigReader
 from os.path import join
-import re
+
 
 directories = ConfigReader().params["directories"]
 TOKENIZED_PATH = directories["tokenized_outputs_dir"]
@@ -10,6 +10,7 @@ RAW_DATA_PATH = directories["raw_outputs_dir"]
 def load_variable(filename):
     file_path = join(RAW_DATA_PATH, filename)
     with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        data = re.findall(r'https?://[^\s,]+', content)
+
+        data_line = file.read().strip()
+        data = data_line.split(',')
     return data
